7d22b33b937beca9701494ece22b6a70536689aa
Monika <77955085+devmonika@users.noreply.github.com>
2023-01-13 00:22:28 +0600

    text center on header
    text center on header

--- src/Pages/HouseCards/HouseCards.js
+++ src/Pages/HouseCards/HouseCards.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const HouseCards = () => {
     return (
         <div className=" min-h-screen">
-            <h1 className='text-5xl font-bold'>House For Rent</h1>
+            <h1 className='text-5xl font-bold text-center'>House For Rent</h1>
             <div className="max-w-screen-xl mx-auto px-4 pt-16 pb-4">
                 <div className="flex flex-col flex-wrap md:flex-row md:-mx-2">
                 <div className="h-72 md:h-96 w-full md:w-1/2 lg:w-1/4 mb-4 lg:mb-0">
