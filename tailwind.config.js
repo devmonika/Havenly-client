b2f86d866b1343af58ecad5f5fc576491037acd9
Monika <77955085+devmonika@users.noreply.github.com>
2023-01-12 08:03:37 +0600

    daisy added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,34 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        havenly: {
+        
+      "primary": "#006F91",
+              
+      "secondary": "#00ABE0",
+              
+      "accent": "#37CDBE",
+              
+      "neutral": "#3D4451",
+              
+      "base-100": "#FFFFFF",
+              
+      "info": "#3ABFF8",
+              
+      "success": "#36D399",
+              
+      "warning": "#FBBD23",
+              
+      "error": "#F87272",
+              },
+            },
+          ],
+        },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
