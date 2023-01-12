723a3726712e0aed213cc3c9a35dc044a335632e
Monika <77955085+devmonika@users.noreply.github.com>
2023-01-12 10:51:12 +0600

    git helmate added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes/Route/Route';
-
+export const siteName = 'Havenly';
 function App() {
   return (
     <div className="App">
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { siteName } from '../../App';
 const Home = () => {
     return (
-        <div>
-            This is home
-        </div>
+        <HelmetProvider>
+            <Helmet>
+				<title>Home - {siteName}</title>
+			</Helmet>
+             <div>
+                This is home
+            </div>
+        </HelmetProvider>
+       
     );
 };
 
