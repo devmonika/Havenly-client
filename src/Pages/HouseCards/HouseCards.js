7844a43869cadec5e1bd496ed7164d4fab305112
Monika <77955085+devmonika@users.noreply.github.com>
2023-01-12 20:32:14 +0600

    Squashed commit of the following:
    commit 340d6470e304bda57e219afffc38e71d868c3afa
    Merge: 723a372 2802def
    Author: md morsalin <108488844+Morsalin3@users.noreply.github.com>
    Date:   Thu Jan 12 12:21:20 2023 +0600
    
        Merge pull request #1 from devmonika/morsalin
    
        added about us
    
    commit 2802defceefde2210b5ebcc2d1562f2ca3a7e06b
    Author: Morsalin3 <mdmorsalin3@gmail.com>
    Date:   Thu Jan 12 12:10:42 2023 +0600
    
        added about us
    
    commit 723a3726712e0aed213cc3c9a35dc044a335632e
    Author: Monika <77955085+devmonika@users.noreply.github.com>
    Date:   Thu Jan 12 10:51:12 2023 +0600
    
        git helmate added

--- src/Pages/HouseCards/HouseCards.js
+++ src/Pages/HouseCards/HouseCards.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HouseCards = () => {
     return (
         <div className=" min-h-screen">
-            <h1 className='text-5xl font-bold text-primary'>Available House</h1>
             <div className="max-w-screen-xl mx-auto px-4 pt-16 pb-4">
                 <div className="flex flex-col flex-wrap md:flex-row md:-mx-2">
                 <div className="h-72 md:h-96 w-full md:w-1/2 lg:w-1/4 mb-4 lg:mb-0">
-                    <a  className="h-72 md:h-96 block group relative mx-2 overflow-hidden shadow-lg">
+                    <Link  className="h-72 md:h-96 block group relative mx-2 overflow-hidden shadow-lg">
                     <img src="https://i.postimg.cc/zvmCkSpW/pexels-max-vakhtbovych-7533926.jpg" className="absolute z-0 object-cover w-full h-72 md:h-96 transform duration-500 group-hover:scale-150" alt=''/>
                     <div className="absolute gradient transition duration-500 group-hover:bg-black group-hover:opacity-90 w-full h-72 md:h-96 z-100"></div>
                     <div className="absolute left-0 right-0 bottom-0 p-6 z-30 transform translate-y-1/2 transition duration-500 h-full group-hover:translate-y-0 delay-100">
@@ -21,10 +21,10 @@ const HouseCards = () => {
                         <button className="bg-white text-black text-sm px-3 py-1 font-semibold opacity-0 transition duration-500 group-hover:opacity-100 border-2 border-white focus:border-black focus:bg-gray-300">Read More</button>
                         </div>
                     </div>
-                    </a>
+                    </Link>
                 </div>
                 <div className="h-72 md:h-96 w-full md:w-1/2 lg:w-1/4 mb-4 md:mb-0">
-                    <a  className="h-72 md:h-96 block group relative mx-2 overflow-hidden shadow-lg border-4 border-blue-500">
+                    <Link  className="h-72 md:h-96 block group relative mx-2 overflow-hidden shadow-lg border-4 border-blue-500">
                     <img src="https://i.postimg.cc/13gDbjT6/pexels-max-vakhtbovych-6970071.jpg" className="absolute z-0 object-cover w-full h-72 md:h-96 transform duration-500 group-hover:scale-150" alt=''/>
                     <div className="absolute gradient transition duration-500 group-hover:bg-black group-hover:opacity-90 w-full h-72 md:h-96 z-10"></div>
                     <div className="absolute left-0 right-0 bottom-0 p-6 z-30 transform translate-y-1/2 transition duration-500 h-full group-hover:translate-y-0 delay-100">
@@ -38,10 +38,10 @@ const HouseCards = () => {
                         <button className="bg-white text-black text-sm px-3 py-1 font-semibold opacity-0 transition duration-500 group-hover:opacity-100 border-2 border-white focus:border-black focus:bg-gray-300">Read More</button>
                         </div>
                     </div>
-                    </a>
+                    </Link>
                 </div>
                 <div className="h-72 md:h-96 w-full md:w-1/2 lg:w-1/4 mb-4 md:mb-0">
-                    <a  className="h-72 md:h-96 block group relative mx-2 overflow-hidden shadow-lg">
+                    <Link  className="h-72 md:h-96 block group relative mx-2 overflow-hidden shadow-lg">
                     <img src="https://i.postimg.cc/W1DfSSwY/pexels-max-vakhtbovych-7005299.jpg" className="absolute z-0 object-cover w-full h-72 md:h-96 transform duration-500 group-hover:scale-150" alt=''/>
                     <div className="absolute gradient transition duration-500 group-hover:bg-black group-hover:opacity-90 w-full h-72 md:h-96 z-10"></div>
                     <div className="absolute left-0 right-0 bottom-0 p-6 z-30 transform translate-y-1/2 transition duration-500 h-full group-hover:translate-y-0 delay-100">
@@ -55,10 +55,10 @@ const HouseCards = () => {
                         <button className="bg-white text-black text-sm px-3 py-1 font-semibold opacity-0 transition duration-500 group-hover:opacity-100 border-2 border-white focus:border-black focus:bg-gray-300">Read More</button>
                         </div>
                     </div>
-                    </a>
+                    </Link>
                 </div>
                 <div className="h-72 md:h-96 w-full md:w-1/2 lg:w-1/4 mb-4 md:mb-0">
-                    <a  className="h-72 md:h-96 block group relative mx-2 overflow-hidden shadow-lg">
+                    <Link  className="h-72 md:h-96 block group relative mx-2 overflow-hidden shadow-lg">
                     <img src="https://i.postimg.cc/g2kZ3k3G/pexels-maria-orlova-4946758.jpg" className="absolute z-0 object-cover w-full h-72 md:h-96 transform duration-500 group-hover:scale-150" alt=''/>
                     <div className="absolute gradient transition duration-500 group-hover:bg-black group-hover:opacity-90 w-full h-72 md:h-96 z-10"></div>
                     <div className="absolute left-0 right-0 bottom-0 p-6 z-30 transform translate-y-1/2 transition duration-500 h-full group-hover:translate-y-0 delay-100">
@@ -72,7 +72,7 @@ const HouseCards = () => {
                         <button className="bg-white text-black text-sm px-3 py-1 font-semibold opacity-0 transition duration-500 group-hover:opacity-100 border-2 border-white focus:border-black focus:bg-gray-300">Read More</button>
                         </div>
                     </div>
-                    </a>
+                    </Link>
                 </div>
                 </div>
             </div>
