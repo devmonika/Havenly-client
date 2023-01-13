a6ddc822d6dad91cf76aa8507680e0900b6f33b2
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2023-01-13 15:18:22 +0600

    lorem-change

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -26,7 +26,7 @@ const Banner = () => {
           <img src={slide} alt="" />
           <div className="slideContent">
             <h2 className="text-3xl md:text-sm font-bold mb-3 ">WELCOME TO OUR HOUSE RENT</h2>
-            <p className="text-xl sm:text-sm ">Lorem ipsum dolor sit amet, ligula magna at etiam aliquip venenatis. Vitae sit felis donec, suscipit tortor et sapien donec ac nec. Nam vel eu amet, nisl imperdiet nec ad morbi, sodales ipsum, consequat purus vitae integer maecenas. Arcu sit, sit velit at ut eu phasellus, tincidunt risus risus scelerisque donec, euismod mollis sit elementum lobortis fermentum lacus.</p>
+            <p className="text-xl sm:text-sm ">As your vacation specialist, our goal is to help you find the perfect Crested Butte rental to fit your needs (and budget). All of our properties are fully furnished, maintained with the utmost care, and ready for your visit. As your vacation rental management specialist, our full-service team will provide you the assurance you need to know your home is in the best care.</p>
            <div className="mt-5 slideBtn">
            <button className="bg-[#FF9524] px-8 py-3 mr-3">Property</button>
             <button className="bg-[#09BE51] px-8 py-3">Contact Us</button>
@@ -37,7 +37,7 @@ const Banner = () => {
           <img src={slide2} alt="" />
           <div className="slideContent">
             <h2 className="text-3xl font-bold mb-3 ">WELCOME TO OUR HOUSE RENT</h2>
-            <p className="text-xl ">Lorem ipsum dolor sit amet, ligula magna at etiam aliquip venenatis. Vitae sit felis donec, suscipit tortor et sapien donec ac nec. Nam vel eu amet, nisl imperdiet nec ad morbi, sodales ipsum, consequat purus vitae integer maecenas. Arcu sit, sit velit at ut eu phasellus, tincidunt risus risus scelerisque donec, euismod mollis sit elementum lobortis fermentum lacus.</p>
+            <p className="text-xl ">As your vacation specialist, our goal is to help you find the perfect Crested Butte rental to fit your needs (and budget). All of our properties are fully furnished, maintained with the utmost care, and ready for your visit. As your vacation rental management specialist, our full-service team will provide you the assurance you need to know your home is in the best care.</p>
            <div className="mt-5">
            <button className="bg-[#FF9524] hover:bg-[#09BE51] px-8 py-3 mr-3">Property</button>
             <button className="bg-[#09BE51] hover:bg-[#FF9524] px-8 py-3">Contact Us</button>
@@ -48,7 +48,7 @@ const Banner = () => {
           <img src={slide} alt="" />
           <div className="slideContent">
             <h2 className="text-3xl md:text-sm font-bold mb-3 ">WELCOME TO OUR HOUSE RENT</h2>
-            <p className="text-xl sm:text-sm ">Lorem ipsum dolor sit amet, ligula magna at etiam aliquip venenatis. Vitae sit felis donec, suscipit tortor et sapien donec ac nec. Nam vel eu amet, nisl imperdiet nec ad morbi, sodales ipsum, consequat purus vitae integer maecenas. Arcu sit, sit velit at ut eu phasellus, tincidunt risus risus scelerisque donec, euismod mollis sit elementum lobortis fermentum lacus.</p>
+            <p className="text-xl sm:text-sm ">As your vacation specialist, our goal is to help you find the perfect Crested Butte rental to fit your needs (and budget). All of our properties are fully furnished, maintained with the utmost care, and ready for your visit. As your vacation rental management specialist, our full-service team will provide you the assurance you need to know your home is in the best care.</p>
            <div className="mt-5 slideBtn">
            <button className="bg-[#FF9524] px-8 py-3 mr-3">Property</button>
             <button className="bg-[#09BE51] px-8 py-3">Contact Us</button>
