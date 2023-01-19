a7f7fa5ffca39378aa4b517537cebb047f5bba2d
Monika <77955085+devmonika@users.noreply.github.com>
2023-01-19 23:32:22 +0600

    private route added and changes header

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -37,7 +37,8 @@ const Header = () => {
 				<a href="/reviews">Reviews</a>
 				<a href="/#">About us</a>
 				<a href="/contactus">Contact</a>
-				<a href="/dashboard">Dashboard</a>
+				
+				
 				<button
 					className="nav-btn nav-close-btn"
 					onClick={showNavbar}>
@@ -48,11 +49,15 @@ const Header = () => {
 				{
 					user?.uid ?
 
-						<button onClick={handleLogOut} className="mr-3">LogOut</button>
+						<>
+						<Link to="/dashboard"  className="mr-3">Dashboard</Link> 
+						<button onClick={handleLogOut} className="mr-3 font-bold">Logout</button>
+						
+						</>
 						:
 						<><Link to='/login' className='mr-3' >Login </Link></>
 				}
-				{/* <Link to='/login' className='mr-3' >Login </Link> */}
+				
 				{
 					user?.displayName ?
 						<p className='mr-3'>{user?.displayName}</p>
@@ -68,9 +73,8 @@ const Header = () => {
 						<FaUserAlt className='mr-3' />
 
 				}
+	
 
-				{/* <FaUserAlt className='mr-3' /> */}
-				{/* <p className='mr-3'>Ibrahim Sikder</p> */}
 				<button className='text-xl ml-3' onClick={() => dispatch(toggleDarkMode())}>
 					{
 						mode ?
@@ -79,7 +83,7 @@ const Header = () => {
 							<FaToggleOn />
 					}
 				</button>
-				{/* <label htmlFor="my-drawer-2" className="nav-btn drawer-button lg:hidden"><FaBars /></label> */}
+				
 			</div>
 			<button className="nav-btn" onClick={showNavbar}>
 				<FaBars />
