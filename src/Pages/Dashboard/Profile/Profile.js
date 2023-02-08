95e4fa5001d42e87520321572d1ab11c57019f02
Monika <77955085+devmonika@users.noreply.github.com>
2023-02-08 15:11:41 +0600

    profile form added

--- src/Pages/Dashboard/Profile/Profile.js
+++ src/Pages/Dashboard/Profile/Profile.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../../contexts/AuthProvider';
 import image from '../../../images/profile.png';
 import { MdEditCalendar } from "react-icons/md";
-
+import { FaCheckCircle } from "react-icons/fa";
 const Profile = () => {
     const { user } = useContext(AuthContext);
     const [userInfo, setuserInfo] = useState([]);
@@ -15,95 +15,104 @@ const Profile = () => {
     }, [user?.email]);
     return (
         <div>
-            <div className='border-t-indigo-500 border-4 mt-2 border-x-0 border-b-0 py-10 bg-white shadow-2xl'>
-                <div className=' flex justify-center items-center  mt-4'>
-                    <img src={userInfo.image} alt="" className='h-60 rounded-full w-60 mt-24' /> <br />
 
+            {/* new cosw */}
+<div class="bg-gray-100 bg-opacity-25 mt-24">
 
-                </div>
-                <div className='text-center mt-2'>
-                    <h5 className='my-2 text-3xl font-semibold text-primary uppercase mt-3'>{userInfo.name}</h5>
+  <div class="lg:w-full bg-purple-600  mb-8">
 
-                    <p className='my-2 leading-8'>responsible for building customer relationships <br /> and developing business with existing and prospective customers <span><MdEditCalendar></MdEditCalendar></span> </p>
-                    <h5 className='my-2'>{userInfo.email}</h5>
-                    <h5>{user?.phoneNumber}</h5>
-                </div>
+    <div class="flex flex-wrap items-center justify-evenly p-4 md:py-8">
+
+      <div class="md:w-3/12 md:ml-16">
+        
+        <img class="w-20 h-20 md:w-40 md:h-40 object-cover rounded-full
+                     border-2 border-pink-600 p-1" src={userInfo.image} alt="profile"/>
+      </div>
+
+      
+      <div class="w-8/12 md:w-7/12 ml-4">
+        <div class="md:flex md:flex-wrap md:items-center mb-4">
+          <h2 class="text-3xl inline-block font-light md:mr-2 mb-2 sm:mb-0">
+          {userInfo.name}
+          </h2>
+
+          
+        
+
+          
+          <a href="#" class=" 
+                        "><FaCheckCircle></FaCheckCircle></a>
+        </div>
+
+        
+      
+
+        
+        <div class="hidden md:block">
+          <h1 class="font-semibold">{userInfo.name}</h1>
+          <span>Travel, Nature and Music</span>
+          <p>Lorem ipsum dolor sit amet consectetur</p>
+        </div>
+
+      </div>
+
+      
+      <div class="md:hidden text-sm my-2">
+        <h1 class="font-semibold">Mr Travlerrr...</h1>
+        <span>Travel, Nature and Music</span>
+        <p>Lorem ipsum dolor sit amet consectetur</p>
+      </div>
+
+    </div>
+
+    
+   
+  </div>
+</div>
+{/* form  */}
+<div class="flex items-center justify-center px-6 w-full">
+    <div class="w-full">
+      <div class="sm:mx-auto sm:w-full sm:max-w-md">
+        {/* <a href="#" class="flex justify-center font-bold text-4xl"> LOGO </a> */}
+
+        <h2 class="mt-6 text-2xl font-extrabold text-center leading-9">Update Your Information</h2>
+
+        
+      </div>
+
+      <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <form>
+          <div>
+            <label for="email" class="block text-sm font-medium leading-5"> Name </label>
+
+            <div class="mt-1 rounded-md shadow-sm">
+              <input id="text" name="text" type="text" required autofocus class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
             </div>
-            <div className='border-t-indigo-500 border-4 mt-4 border-x-0 border-b-0 py-10 bg-white shadow-2xl'>
-                <h5 className='text-xl mb-2'>Profile Update</h5>
-                <div>
-                    <form >
-                        <div className="md:col-span-2">
-                            <label htmlFor="contact">Name</label>
-                            <div className="h-10 flex border border-gray-200 rounded items-center mt-1">
-                                <input type="number" name="name" id="contact" placeholder="Contact" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
+          </div>
 
-                                />
-                            </div>
-                        </div>
-                        <div className="md:col-span-2 my-2">
-                            <label htmlFor="contact">Email</label>
-                            <div className="h-10 flex border border-gray-200 rounded items-center mt-1">
-                                <input type="number" name="email" id="contact" placeholder="Contact" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
+          <div class="mt-6">
+            <label for="password" class="block text-sm font-medium text-gray-700 leading-5"> image </label>
 
-                                />
-                            </div>
-                        </div>
-                        <div className="md:col-span-2 my-2">
-                            <label htmlFor="contact">Address</label>
-                            <div className="h-10 flex border border-gray-200 rounded items-center mt-1">
-                                <input type="number" name="address" id="contact" placeholder="Address" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
-
-                                />
-                            </div>
-                        </div>
-                        <div className="md:col-span-5 my-2">
-                            <label htmlFor="description">About Us</label>
-                            <textarea className="textarea w-full pb-8 border-gray-300"
-                                placeholder="About us"
-                                name='about'
-
-                            ></textarea>
-                        </div>
-                        {/* <div className="md:col-span-2 my-2">
-                            <label htmlFor="contact">Reviews</label>
-                            <div className="h-10 flex border border-gray-200 rounded items-center mt-1">
-                                <input type="number" name="reviews" id="reviews" placeholder="Add Your Reviews" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
-
-                                />
-                            </div>
-                        </div> */}
-                        <div className="md:col-span-2 my-2">
-                            <label htmlFor="contact">Ratings</label>
-                            <div className="h-10 flex border border-gray-200 rounded items-center mt-1">
-                                <input type="number" name="ratings" id="ratings" placeholder="Rate our sevices" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
-
-                                />
-                            </div>
-                        </div>
-
-                        <div className="md:col-span-2 my-2">
-                            <label htmlFor="contact">Phone</label>
-                            <div className="h-10 flex border border-gray-200 rounded items-center mt-1">
-                                <input type="number" name="phone" id="contact" placeholder="Contact" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
-
-                                />
-                            </div>
-                        </div>
-                    </form>
-                    <div className='md:col-span-5 text-center mt-2'>
-                        <div>
-                            <input type="submit" value="submit" className='bg-gradient-to-r from-purple-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 text-white font-bold py-2 px-6 rounded' />
-                        </div>
-
-                    </div>
-                    {/* <div className="md:col-span-5 text-right">
-                        <div className="inline-flex items-start">
-                            <input className="bg-gradient-to-r from-purple-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 text-white font-bold py-2 px-4 rounded" type="submit" value="Add Properties" />
-                        </div>
-                    </div> */}
-                </div>
+            <div class="mt-1 rounded-md shadow-sm">
+              <input id="text" type="file" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
             </div>
+          </div>
+          <div class="mt-6">
+            <label for="password" class="block text-sm font-medium text-gray-700 leading-5"> About </label>
+
+            <div class="mt-1 rounded-md shadow-sm">
+              <textarea id="text" type="text" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 transition duration-150 ease-in-out sm:text-sm sm:leading-5"></textarea>
+            </div>
+          </div>
+          <div class="mt-6">
+            <span class="block w-full rounded-md shadow-sm">
+              <button type="submit" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-red-600 border border-transparent rounded-md hover:bg-red-500 focus:outline-none transition duration-150 ease-in-out">Update</button>
+            </span>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
         </div>
     );
 };
