31773fbf7c764c4197975c5bd0299749eed7ff44
Monika <77955085+devmonika@users.noreply.github.com>
2023-02-10 16:50:11 +0600

    changes

--- src/Pages/Dashboard/Profile/Profile.js
+++ src/Pages/Dashboard/Profile/Profile.js
@@ -51,22 +51,22 @@ const Profile = () => {
         <div>
 
             {/* new cosw */}
-<div class="bg-gray-100 bg-opacity-25 mt-24">
+<div class="border-gray-700 bg-opacity-25 mt-24">
 
-  <div class="lg:w-full bg-purple-600  mb-8">
+  <div class="lg:w-full bg-primary mb-8">
 
     <div class="flex flex-wrap items-center justify-evenly p-4 md:py-8">
 
       <div class="md:w-3/12 md:ml-16">
         
         <img class="w-20 h-20 md:w-40 md:h-40 object-cover rounded-full
-                     border-2 border-pink-600 p-1" src={userInfo.image} alt="profile"/>
+                     border-2 border-secondary p-1" src={userInfo.image} alt="profile"/>
       </div>
 
       
       <div class="w-8/12 md:w-7/12 ml-4">
         <div class="md:flex md:flex-wrap md:items-center mb-4">
-          <h2 class="text-3xl inline-block font-light md:mr-2 mb-2 sm:mb-0">
+          <h2 class="text-3xl inline-block font-light md:mr-2 mb-2 sm:mb-0 text-white">
           {userInfo.name}
           </h2>
 
@@ -74,15 +74,14 @@ const Profile = () => {
         
 
           
-          <a href="#" class=" 
-                        "><FaCheckCircle></FaCheckCircle></a>
+          <a href="#" class="text-blue-600 text-xl"><FaCheckCircle></FaCheckCircle></a>
         </div>
 
         
       
 
         
-        <div class="hidden md:block">
+        <div class="hidden md:block text-white">
           <h1 class="font-semibold">{userInfo.name}</h1>
           <span>Travel, Nature and Music</span>
           <p>Lorem ipsum dolor sit amet consectetur</p>
@@ -120,7 +119,7 @@ const Profile = () => {
             <label for="email" class="block text-sm font-medium leading-5"> Name </label>
 
             <div class="mt-1 rounded-md shadow-sm">
-              <input id="text" name="text" type="text" required autofocus class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+              <input id="text" name="text" type="text" required autofocus class="appearance-none block w-full px-3 py-2 border border-secondary rounded-md placeholder-gray-400 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
             </div>
           </div>
 
@@ -128,19 +127,19 @@ const Profile = () => {
             <label for="password" class="block text-sm font-medium text-gray-700 leading-5"> image </label>
 
             <div class="mt-1 rounded-md shadow-sm">
-              <input id="text" type="file" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+              <input id="text" type="file" required class="appearance-none block w-full px-3 py-2 border border-secondary rounded-md placeholder-gray-400 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
             </div>
           </div>
           <div class="mt-6">
             <label for="password" class="block text-sm font-medium text-gray-700 leading-5"> About </label>
 
             <div class="mt-1 rounded-md shadow-sm">
-              <textarea id="text" type="text" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 transition duration-150 ease-in-out sm:text-sm sm:leading-5"></textarea>
+              <textarea id="text" type="text" required class="appearance-none block w-full px-3 py-2 border border-secondary rounded-md placeholder-gray-400 transition duration-150 ease-in-out sm:text-sm sm:leading-5"></textarea>
             </div>
           </div>
           <div class="mt-6">
             <span class="block w-full rounded-md shadow-sm">
-              <button type="submit" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-red-600 border border-transparent rounded-md hover:bg-red-500 focus:outline-none transition duration-150 ease-in-out">Update</button>
+              <button type="submit" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-secondary border border-transparent rounded-md hover:bg-primary focus:outline-none transition duration-150 ease-in-out mb-24">Update</button>
             </span>
           </div>
         </form>
