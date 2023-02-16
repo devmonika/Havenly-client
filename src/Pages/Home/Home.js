e40baff92c81573d3ad2d6ac27ecb11bae2bdc53
Morsalin3 <mdmorsalin3@gmail.com>
2023-02-16 12:14:36 +0600

    added ad

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,11 +7,8 @@ import PopulerPlaces from "../PopulerPlacesCard/PopulerPlaces";
 import Cta from "../Cta/Cta";
 import HouseCards from "../HouseCards/HouseCards";
 import Testimonials from "../Testimonials/Testimonials";
-<<<<<<< HEAD
 import Advertise from "../Advertise/Advertise";
-=======
 import OurDashboard from "../OurDashboard/OurDashboard";
->>>>>>> d7f546ec131678e2b3405d2b017c2d1aaa7693b0
 const Home = () => {
   return (
     <HelmetProvider>
