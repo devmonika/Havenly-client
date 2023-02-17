3e02b1423353441ab07ecaa6c4b2708a22dde7cf
Monika <77955085+devmonika@users.noreply.github.com>
2023-02-17 20:21:48 +0600

    changed

--- src/Pages/Advertise/Advertise.js
+++ src/Pages/Advertise/Advertise.js
@@ -21,8 +21,8 @@ const Advertise = () => {
 
     return (
 
-        <div>
-                <h2 className='text-center text-4xl font-bold leading-normal mt-4 mb-10'>Apartments</h2>
+        <div className='mb-24'>
+                <h2 className='text-center text-4xl font-bold leading-normal mt-4 mb-10'>Features Apertments</h2>
             {/* card section start here  */}
             {isLoading && <Loading></Loading>}
             <div className="wrapProperty">
