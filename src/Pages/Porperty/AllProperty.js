20aaf3d79ba2029cd883426efbe8c574b12183e6
tauhid880 <tauhid880@gmail.com>
2023-02-22 03:50:52 +0600

    Search & filter added and make some design change

--- src/Pages/Porperty/AllProperty.js
+++ src/Pages/Porperty/AllProperty.js
@@ -7,49 +7,47 @@ import { siteName } from "../../App";
 import { useQuery } from "@tanstack/react-query";
 import PropertyDetails from "./PropertyDetails";
 import Loading from "../Shared/Footer/Loading/Loading";
+import Search from "../../components/Search";
 // import { useGlobalContext } from "../../contexts/SearchProvider";
 // import Search from "../../components/Search";
 
 const AllProperty = () => {
-  const [category, setCategory] = useState('Residential');
-  const [search, setSearch] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("all");
 
-
-  const { data: properties = [], refetch, isLoading } = useQuery({
-    queryKey: ['property', category],
-    queryFn: async () => {
-      const res = await fetch(`https://havenly-server-new.vercel.app/properties/property/${category}`);
-      const data = await res.json();
-      return data;
-    },
-  });
-
-  // const searchItem = properties.filter((item) => {
-  //   if (search === "") {
-  //     console.log(item.category);
-  //     return item.category;
-  //   }
-  // });
-
-  const { data: categories = [] } = useQuery({
-    queryKey: ["categories"],
+  const {
+    data: apartments = [],
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: ["property"],
     queryFn: async () => {
       const res = await fetch(
-        "https://havenly-server-new.vercel.app/categories"
+        `https://havenly-server-new.vercel.app/properties`
       );
       const data = await res.json();
       return data;
     },
   });
 
-  const handleCategoryChange = (event) => {
-    event.preventDefault();
-    setCategory(event.target.value);
-
-    refetch();
+  const handleSearchTermChange = (event) => {
+    setSearchTerm(event.target.value);
   };
 
-  // console.log(category);
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const filteredApartments = apartments.filter((apartment) => {
+    if (selectedCategory === "all") {
+      return apartment.address.toLowerCase().includes(searchTerm.toLowerCase());
+    } else {
+      return (
+        apartment.category === selectedCategory &&
+        apartment.city.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+  });
 
   return (
     <HelmetProvider>
@@ -63,47 +61,28 @@ const AllProperty = () => {
             <h2>Apartments</h2>
           </div>
         </div>
-        <div className="flex flex-row gap-10 items-center justify-center relative mb-10 mt-16 searchWrap">
-          {/* Location input */}
-          {/* <form onSubmit={(e) => e.preventDefault()}>
-            <input
-              className="input input-bordered w-full max-w-xs"
-              type="text"
-              value={query}
-              placeholder="Search Location"
-              onChange={(event) => searchLocation(event.target.value)}
-            />
-          </form> */}
-          {/* Location input End */}
-          {/* <Search></Search> */}
-          <form className="">
-            <select
-              className="select select-bordered w-full max-w-xs text-black"
-              name=""
-              id=""
-              onChange={handleCategoryChange}
-            >
-              {categories.map((category) => (
-                <option key={category._id} value={category.categoryName}>
-                  {category.categoryName}
-                </option>
-              ))}
-            </select>
-          </form>
-        </div>
+        {/* Search component start */}
+        <Search
+          handleSearchTermChange={handleSearchTermChange}
+          handleCategoryChange={handleCategoryChange}
+          searchTerm={searchTerm}
+          selectedCategory={selectedCategory}
+        ></Search>
+        {/* Search component end */}
+
         {/* card section start here  */}
         {isLoading && <Loading></Loading>}
         <div className="wrapProperty">
           <div className="singleProperty">
-            <div className="leftSide mr-[16px]">
-              {properties.map((property) => (
+            <div className="leftSide mt-20 mr-[16px]">
+              {filteredApartments.map((property) => (
                 <PropertyDetails
                   key={property._id}
                   property={property}
                 ></PropertyDetails>
               ))}
             </div>
-            <div></div>   
+            <div></div>
           </div>
         </div>
       </div>
